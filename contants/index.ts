94a78834a29fd671bd6b17b2c9cb9d88bf1f8d10
haseeb5555 <haseebmoon666@gmail.com>
2024-11-18 14:25:42 +0500

    feat:more starters

--- contants/index.ts
+++ contants/index.ts
@@ -1,121 +1,173 @@
-export const starterProjects=[
-    {
-        name:"Admin Dashboard ",
-        description:"Admin Dashboard built with Nextjs , Shadcn and Tailwind CSS",
-        url:"https://github.com/Kiranism/next-shadcn-dashboard-starter",
-        creator:"Kiran",
-        category:"Frontend",
+export const starterProjects = [
+  {
+    name: "Paddle Nextjs Starter",
+    description:
+      "Paddle Next.js Starter Template with Pricing, Authentication, and more",
+    url: "https://github.com/PaddleHQ/paddle-nextjs-starter-kit",
+    creator: "Paddle",
+    category: "Frontend",
+  },
+  {
+    name: "Medusa Starter",
+    description: "A starter project for Medusa, the headless commerce engine",
+    url: "https://github.com/medusajs/medusa",
+    creator: "Medusa",
+    category: "Frontend",
+  },
+  {
+    name: "Nextra Docs Starter",
+    description: "Documentation starter for Nextjs with MDX and Tailwind CSS",
+    url: "https://github.com/shuding/nextra-docs-template",
+    creator: "Shuding",
+    category: "Frontend",
+  },
 
-    },{
-        name:"Next js Landig Page",
-        description:"Next js Landing Page built with Tailwind CSS",
-        url:"https://github.com/nobruf/shadcn-landing-page",
-        creator:"nobruf",
-        category:"Frontend",
-    },
-    {
-        name:"Travel Landing Page build with Next JS",
-        description:"Travel Landing Page built with Next JS",
-        url:"https://github.com/shabrina12/Travel-Landing-Page",
-        creator:"Shabrina",
-        category:"Frontend",
-    },{
-        name:"Next js Admin Dashbaord",
-        description:"Dashboard starter using NextUI V2 and Nextjs",
-        url:"https://github.com/Siumauricio/nextui-dashboard-template",
-        creator:"Mauricio",
-        category:"Frontend",
+  {
+    name: "T3 Nextjs Starter",
+    description:
+      "T3 Nextjs Starter is a Next.js starter template that includes TypeScript, Tailwind CSS, and TRPC Drizzle ORM.",
+    url: "https://github.com/t3-oss/create-t3-app",
+    creator: "Theo",
+    category: "Fullstack",
+  },
+  {
+    name: "Next Faster",
+    description: "A highly performant e-commerce template using Next.js",
+    url: "https://github.com/ethanniser/NextFaster",
+    creator: "Ethan",
+    category: "Frontend",
+  },
 
-    },{
-        name:"React+Vite Admin Dashboard",
-        description:"Admin Dashboard built with React and Vite and Tailwind CSS" ,
-        url:"https://github.com/Kiranism/react-shadcn-dashboard-starter",
-        creator:"Kiran",
-        category:"Frontend",
-    },{
-        name:"React Vite Landing Page",
-        description:"React Landing Page built with Tailwind CSS",
-        url:"https://github.com/leoMirandaa/shadcn-landing-page",
-        creator:"Leo",
-        category:"Frontend",
-    },{
-        name:"E Commerce Starter",
-        description:"E Commerce Starter built with Nextjs and Tailwind CSS",
-        url:"https://github.com/vercel/commerce",
-        creator:"Vercel",
-        category:"Frontend",
-        
-    },{
-        name:"Next js E Commerce",
-        description:"E Commerce Starter built with Nextjs and Tailwind CSS",
-        url:"https://github.com/medusajs/nextjs-starter-medusa",
-        creator:"Medusa",
-        category:"Frontend",
-       
-    },{
-        name:"Next js E-Commerce",
-        description:"E Commerce Starter built with Nextjs and Tailwind CSS",
-        url:"https://github.com/Blazity/enterprise-commerce",
-        creator:"Blazity",
-        category:"Frontend",
-    },{
-        name:"Next SaaS Starter",
-        description:"Get started quickly with Next.js, Postgres, Stripe, and shadcn/ui."
-        ,
-        url:"https://github.com/leerob/next-saas-starter",
-        creator:"Lee Robinson",
-        category:"Frontend",
-    },{
-        name:"NextJs SaaS Starter",
-        description:"Open-source SaaS Starter with User Roles & Admin Panel. Built using Next.js 14, Prisma, Neon, Auth.js v5, Resend, React Email, Shadcn/ui, Stripe, Server Actions.",
-        url:"https://github.com/mickasmt/next-saas-stripe-starter",
-        creator:"Mickael",
-        category:"Frontend",
-    },{
-        name:"Next js Blog Starter",
-        description:"Blog Starter built with Nextjs and Tailwind CSS",
-        url:"https://github.com/timlrx/tailwind-nextjs-starter-blog",
-        creator:"Tim",
-        category:"Frontend",
-    },
-    {
-        name:"Nextjs-starter-prismic-blog",
-        description:"Blog Starter built with Nextjs and Tailwind CSS",
-        url:"https://github.com/prismicio-community/nextjs-starter-prismic-blog",
-        creator:"Prismic",
-        category:"Frontend",
-    },{
-        name:"Node Typescript Starter",
-        description:"A production ready Node.js (typescript) template for your next project",
-        url:"https://github.com/fless-lab/node-ts-starter",
-        creator:"Fless",
-        category:"Backend",
-    },{
-        name:"Express Typescript Starter",
-        description:"Express + TypeScript + Boilerplate for Web / API Applications",
-        url:"https://github.com/edwinhern/express-typescript-2024",
-        creator:"Edwin",
-        category:"Backend",
-
-    },{
-        name:"Fastify React Starter",
-        description:"Fastify + React + TypeScript + Boilerplate for Web / API Applications",
-        url:"https://github.com/teziapp/fastify-react-starter",
-        creator:"Tezi",
-        category:"Backend",
-    },{
-        name:"Josh Stack",
-        description:"Confidently ship high-performance, low-cost Next.js apps",
-        url:"https://github.com/upstash/jstack",
-        creator:"Josh",
-        category:"Fullstack",
-
-    },{
-        name:"Django Starter",
-        description:"Django Starter Kit: Simplifying Development with Docker Compose, Celery, ElasticSearch, PostgreSQL, and Swagger ",
-        url:"https://github.com/codewithmuh/django-starter-kit",
-        creator:"Muh",
-        category:"Backend",
-    }
-]
-
+  {
+    name: "Admin Dashboard ",
+    description: "Admin Dashboard built with Nextjs , Shadcn and Tailwind CSS",
+    url: "https://github.com/Kiranism/next-shadcn-dashboard-starter",
+    creator: "Kiran",
+    category: "Frontend",
+  },
+  {
+    name: "Next js Landig Page",
+    description: "Next js Landing Page built with Tailwind CSS",
+    url: "https://github.com/nobruf/shadcn-landing-page",
+    creator: "nobruf",
+    category: "Frontend",
+  },
+  {
+    name: "Travel Landing Page build with Next JS",
+    description: "Travel Landing Page built with Next JS",
+    url: "https://github.com/shabrina12/Travel-Landing-Page",
+    creator: "Shabrina",
+    category: "Frontend",
+  },
+  {
+    name: "Next js Admin Dashbaord",
+    description: "Dashboard starter using NextUI V2 and Nextjs",
+    url: "https://github.com/Siumauricio/nextui-dashboard-template",
+    creator: "Mauricio",
+    category: "Frontend",
+  },
+  {
+    name: "React+Vite Admin Dashboard",
+    description: "Admin Dashboard built with React and Vite and Tailwind CSS",
+    url: "https://github.com/Kiranism/react-shadcn-dashboard-starter",
+    creator: "Kiran",
+    category: "Frontend",
+  },
+  {
+    name: "React Vite Landing Page",
+    description: "React Landing Page built with Tailwind CSS",
+    url: "https://github.com/leoMirandaa/shadcn-landing-page",
+    creator: "Leo",
+    category: "Frontend",
+  },
+  {
+    name: "E Commerce Starter",
+    description: "E Commerce Starter built with Nextjs and Tailwind CSS",
+    url: "https://github.com/vercel/commerce",
+    creator: "Vercel",
+    category: "Frontend",
+  },
+  {
+    name: "Next js E Commerce",
+    description: "E Commerce Starter built with Nextjs and Tailwind CSS",
+    url: "https://github.com/medusajs/nextjs-starter-medusa",
+    creator: "Medusa",
+    category: "Frontend",
+  },
+  {
+    name: "Next js E-Commerce",
+    description: "E Commerce Starter built with Nextjs and Tailwind CSS",
+    url: "https://github.com/Blazity/enterprise-commerce",
+    creator: "Blazity",
+    category: "Frontend",
+  },
+  {
+    name: "Next SaaS Starter",
+    description:
+      "Get started quickly with Next.js, Postgres, Stripe, and shadcn/ui.",
+    url: "https://github.com/leerob/next-saas-starter",
+    creator: "Lee Robinson",
+    category: "Frontend",
+  },
+  {
+    name: "NextJs SaaS Starter",
+    description:
+      "Open-source SaaS Starter with User Roles & Admin Panel. Built using Next.js 14, Prisma, Neon, Auth.js v5, Resend, React Email, Shadcn/ui, Stripe, Server Actions.",
+    url: "https://github.com/mickasmt/next-saas-stripe-starter",
+    creator: "Mickael",
+    category: "Frontend",
+  },
+  {
+    name: "Next js Blog Starter",
+    description: "Blog Starter built with Nextjs and Tailwind CSS",
+    url: "https://github.com/timlrx/tailwind-nextjs-starter-blog",
+    creator: "Tim",
+    category: "Frontend",
+  },
+  {
+    name: "Nextjs-starter-prismic-blog",
+    description: "Blog Starter built with Nextjs and Tailwind CSS",
+    url: "https://github.com/prismicio-community/nextjs-starter-prismic-blog",
+    creator: "Prismic",
+    category: "Frontend",
+  },
+  {
+    name: "Node Typescript Starter",
+    description:
+      "A production ready Node.js (typescript) template for your next project",
+    url: "https://github.com/fless-lab/node-ts-starter",
+    creator: "Fless",
+    category: "Backend",
+  },
+  {
+    name: "Express Typescript Starter",
+    description:
+      "Express + TypeScript + Boilerplate for Web / API Applications",
+    url: "https://github.com/edwinhern/express-typescript-2024",
+    creator: "Edwin",
+    category: "Backend",
+  },
+  {
+    name: "Fastify React Starter",
+    description:
+      "Fastify + React + TypeScript + Boilerplate for Web / API Applications",
+    url: "https://github.com/teziapp/fastify-react-starter",
+    creator: "Tezi",
+    category: "Backend",
+  },
+  {
+    name: "Josh Stack",
+    description: "Confidently ship high-performance, low-cost Next.js apps",
+    url: "https://github.com/upstash/jstack",
+    creator: "Josh",
+    category: "Fullstack",
+  },
+  {
+    name: "Django Starter",
+    description:
+      "Django Starter Kit: Simplifying Development with Docker Compose, Celery, ElasticSearch, PostgreSQL, and Swagger ",
+    url: "https://github.com/codewithmuh/django-starter-kit",
+    creator: "Muh",
+    category: "Backend",
+  },
+];
